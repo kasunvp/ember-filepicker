bc37f70fb5bfd458731c733748355598af88f95f
Kasun Vidanapathirana <kasunvp@gmail.com>
2016-07-09 10:53:17 +0530

    refactored filepicker component and added class and ability to hide the upload-button

--- addon/components/ember-filepicker.js
+++ addon/components/ember-filepicker.js
@@ -3,24 +3,28 @@ import Ember from 'ember';
 import layout from '../templates/components/ember-filepicker';
 
 const { Component } = Ember;
+const FILEPICKER_METHOD_PICK = 'pick';
+const FILEPICKER_METHOD_PICK_MULTIPLE = 'pickMultiple';
 
 export default Component.extend({
   buttonText: 'Upload',
   layout,
   multiple: false,
   pickerOptions: {},
+  showUploadButton: true,
+
   actions: {
     selectFile() {
+      let method = FILEPICKER_METHOD_PICK_MULTIPLE;
       if (this.get('multiple')) {
-        filepicker.pickMultiple(this.get('pickerOptions'));
-      } else {
-        filepicker.pick(
-          this.get('pickerOptions'),
-          (file)=> {
-            this.get('onFileSelected')(file);
-          }
-        );
+        method = FILEPICKER_METHOD_PICK;
       }
+      filepicker.method(
+        this.get('pickerOptions'),
+        (file)=> {
+          this.get('onFileSelected')(file);
+        }
+      );
     }
   }
 });
